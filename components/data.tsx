dd7a939e8df72f4ff94cdb413f629c7ed75318a1
owolfdev <owolfdev@gmail.com>
2024-01-19 05:01:07 +0700

    update pagination

--- components/data.tsx
+++ components/data.tsx
@@ -20,7 +20,10 @@ async function Data({
       <div>Total Number of Records: {records}</div>
       {employees.length > 0 ? (
         employees.map((employee) => (
-          <div key={employee.id} className="border rounded p-4 w-[360px]">
+          <div
+            key={employee.id}
+            className="border rounded p-4 w-[360px] border-gray-500"
+          >
             <p>First Name: {employee.first_name}</p>
             <p>Last Name: {employee.last_name}</p>
             <p>Email: {employee.email}</p>
